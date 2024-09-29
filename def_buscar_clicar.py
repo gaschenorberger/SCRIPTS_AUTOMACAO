4d4bfe97f47d18d921cce1e3d2d224e7dd87de80
Gabriel Alvise <148637079+Alvisera@users.noreply.github.com>
2024-09-28 23:53:36 -0300

    Update def_buscar_clicar.py

--- def_buscar_clicar.py
+++ def_buscar_clicar.py
@@ -1,17 +1,18 @@
 #TEM COMO FUNÇÃO BUSCAR UMA PALAVRA NA TELA E CLICAR EM CIMA DELA
 
 #pip install pyautogui pytesseract pillow
+#https://github.com/UB-Mannheim/tesseract/wiki -- instalar tesseract
+#https://github.com/tesseract-ocr/tessdata/blob/main/por.traineddata -- idioma português
 
 import pyautogui
 import pytesseract
 from PIL import ImageGrab
 
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'  #https://github.com/UB-Mannheim/tesseract/wiki -- instalar tesseract
-
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'  
 def buscar_e_clicar(palavra):
     screenshot = ImageGrab.grab()
     
-    texto_tela = pytesseract.image_to_data(screenshot, output_type=pytesseract.Output.DICT)
+    texto_tela = pytesseract.image_to_data(screenshot, lang='por', output_type=pytesseract.Output.DICT)
     
     for i, palavra_tela in enumerate(texto_tela['text']):
         if palavra.lower() == palavra_tela.lower():
