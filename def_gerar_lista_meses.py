cca2dd948392f40931099869d3c89ff0fe00ce4e
Gabriel Alvise <148637079+Alvisera@users.noreply.github.com>
2024-10-24 16:01:53 -0300

    Update def_gerar_lista_meses.py

--- def_gerar_lista_meses.py
+++ def_gerar_lista_meses.py
@@ -1,17 +1,18 @@
 import calendar
 from datetime import datetime
 
-def gerar_lista_meses(ano_inicio, ano_fim):
+def gerar_lista_meses(ano_inicio, ano_fim, mes_inicio=1):
     lista_meses = []
     for ano in range(ano_inicio, ano_fim + 1):
-        for mes in range(1, 13):
+        for mes in range(mes_inicio, 13):
             inicio_mes = f"01/{mes:02d}/{ano}"
             ultimo_dia_mes = calendar.monthrange(ano, mes)[1]
             fim_mes = f"{ultimo_dia_mes:02d}/{mes:02d}/{ano}"
             lista_meses.append((inicio_mes, fim_mes))
+        mes_inicio = 1 
     return lista_meses
 
-lista_meses = gerar_lista_meses(2018, 2024)
+lista_meses = gerar_lista_meses(2018, 2024, 8)
     for inicio, fim in lista_meses:
         print(f"'{inicio}', '{fim}'") #01/01/2018 -- 31/01/2018
 
