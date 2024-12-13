0f35e20378a4fc3dc4cba5c4b65becd773c4d17b
Gabriel Alvise <148637079+Alvisera@users.noreply.github.com>
2024-12-13 17:31:09 -0300

    Update def_procurar_imagem_clicar.py

--- def_procurar_imagem_clicar.py
+++ def_procurar_imagem_clicar.py
@@ -1,12 +1,6 @@
-import pyautogui
-import time
-import random
-import mousekey
+def procurarImagem(nome_arquivo, confidence=0.8, region=None, maxTentativas=60, horizontal=0, vertical=0, dx=0, dy=0, acao='clicar', clicks=1, ocorrencia=1, delay_tentativa=1):
+    mkey = mousekey.MouseKey()
 
-mkey = mousekey.MouseKey()
-
-def procurar_imagem(nome_arquivo, confidence=0.8, region=None, max_tentativas=60, horizontal=0, vertical=0, acao='clicar', clicks=1):
-    
     def click(x, y):
         pyautogui.click(x, y)
 
@@ -16,7 +10,7 @@ def procurar_imagem(nome_arquivo, confidence=0.8, region=None, max_tentativas=60
     def coordenada(x, y):
         print(f'Coordenadas da imagem: ({x}, {y})')
 
-    def move_mouse(x,y,variationx=(-5, 5),variationy=(-5, 5),up_down=(0.2, 0.3),min_variation=-10,max_variation=10,use_every=4,sleeptime=(0.009, 0.019),linear=90,):
+    def moveMouse(x, y, variationx=(-5, 5), variationy=(-5, 5), up_down=(0.2, 0.3), min_variation=-10, max_variation=10, use_every=4, sleeptime=(0.009, 0.019), linear=90):
         mkey.left_click_xy_natural(
             int(x) - random.randint(*variationx),
             int(y) - random.randint(*variationy),
@@ -28,36 +22,56 @@ def procurar_imagem(nome_arquivo, confidence=0.8, region=None, max_tentativas=60
             print_coords=True,
             percent=linear,
         )
-    
-    acoes_validas = ['clicar', 'mover_clicar']
 
-    if acao not in acoes_validas:
-        raise ValueError(f"Ação inválida: '{acao}'. Escolha entre {acoes_validas}.")
+    def clickDrag(x, y, dx, dy):
+        pyautogui.moveTo(x, y)
+        pyautogui.mouseDown()
+        pyautogui.moveTo(x + dx, y + dy, duration=0.5)
+        pyautogui.mouseUp()
+
+
+    acoesValidas = ['clicar', 'mover clicar', 'clicar arrastar']
+
+    if acao not in acoesValidas:
+        raise ValueError(f"Ação inválida: '{acao}'. Escolha entre {acoesValidas}.")
 
     tentativas = 0
-
-    while tentativas < max_tentativas:
+    while tentativas < maxTentativas:
         tentativas += 1
         try:
-            img = pyautogui.locateCenterOnScreen(nome_arquivo, confidence=confidence, region=region)
-            if img:
-                x, y = img
-                x += horizontal
-                y += vertical
-                #coordenada(x, y)
-                if acao == 'clicar':
-                    if clicks == 1:
-                        click(x, y)
-                    else:
-                        doubleClick(x, y)
-                elif acao == 'mover_clicar':
-                    move_mouse(x, y)
-                return True
-        except pyautogui.ImageNotFoundException:
+            imag = list(pyautogui.locateAllOnScreen(nome_arquivo, confidence=confidence, region=region))
+            
+            if imag:
+                if len(imag) >= ocorrencia:
+                    img = imag[ocorrencia - 1]  
+                    x, y = pyautogui.center(img) 
+                    x += horizontal
+                    y += vertical
+
+                    match acao:
+                        case 'clicar':
+                            match clicks:
+                                case 0:
+                                    coordenada(x, y)
+                                case 1:
+                                    click(x, y)
+                                case 2:
+                                    doubleClick(x, y)
+
+                        case 'mover clicar':
+                            moveMouse(x, y)
+                        
+                        case 'clicar arrastar':
+                            clickDrag(x, y, dx, dy)
+
+                    return True
+                else:
+                    print(f'A ocorrência {ocorrencia} não foi encontrada.')
+                    return False
+
+        except pyscreeze.ImageNotFoundException:
             pass
-        time.sleep(1)
+        time.sleep(delay_tentativa)
 
-    print(f'Imagem não encontrada após {max_tentativas} segundos.')
+    print(f'Imagem não encontrada após {maxTentativas} tentativas.')
     return False
-
-procurar_imagem('x.png')
