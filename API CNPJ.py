aa76df3a7d34cd38f2e735e61aad216282597a9e
Gabriel Alvise <148637079+gaschenorberger@users.noreply.github.com>
2025-07-31 16:04:45 -0300

    Create API CNPJ.py

---
+++ API CNPJ.py
@@ -0,0 +1,27 @@
+import json
+import http
+import time
+
+# API CNPJ
+    def get_cnpj_data(cnpj):
+        conn = http.client.HTTPSConnection("receitaws.com.br")
+        headers = { 'Accept': "application/json" }
+        
+        while True:
+            try:
+                conn.request("GET", f"/v1/cnpj/{cnpj}", headers=headers)
+                res = conn.getresponse()
+                data = res.read()
+                json_data = json.loads(data.decode("utf-8"))
+                
+                # Se status == OK, deu certo, então retorna
+                if json_data.get("status") == "OK":
+                    print("API OK")
+                    return json_data
+                else:
+                    print(f"[AVISO] Consulta falhou ou atingiu limite. Tentando de novo em 20 segundos... Resposta: {json_data}")
+                    time.sleep(20)
+
+            except Exception as e:
+                print(f"[ERRO] Falha ao consultar CNPJ: {e}. Tentando de novo em 20 segundos...")
+                time.sleep(20)
