b59154eb49b281d5635407872a2cff66446aaa1a
Gabriel Alvise <148637079+gaschenorberger@users.noreply.github.com>
2025-07-31 16:28:49 -0300

    Update def_marcarInicio.py

--- def_marcarInicio.py
+++ def_marcarInicio.py
@@ -1,5 +1,15 @@
+from datetime import datetime
+
 # Marcar inicio da execução do robô
 
 def marcarInicio():
     inicioExe = datetime.now()
     print(f"Robô iniciado em: {inicioExe.strftime('%d/%m/%Y %H:%M:%S')}")
+
+
+
+# OBTER DIA ATUAL (31/07/2024)
+dataAtual = datetime.now()
+dataAtual = dataAtual.strftime('%d/%m/%Y')
+print(dataAtual)
+
